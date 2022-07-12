2996420ddb3d7d712ebe1193abb6d20d70500ffe
Rizky Rosyid Hidayat <ocid.271198@gmail.com>
2022-07-13 06:56:51 +0700

    add base url

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    BASE_URL: 'https://my-json-server.typicode.com/rizkyRosyidHidayat/shoes-store-db'
+  }
 }
 
 module.exports = nextConfig
